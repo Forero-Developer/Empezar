aedc474ea6f86c24a91f07cd7c6dcb541fc2da18
jcgaleanog <44102395+jcgaleanog@users.noreply.github.com>
2023-10-04 12:24:24 -0500

    Nuevo Codigo

---
+++ src/ejemplopresentacion/EjemploPresentacion.java
@@ -0,0 +1,43 @@
+
+/*
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                      MINIPROYECTO 1
+//
+//
+//          Nombre:     Ejemplo de Presentación curso
+//          Fecha:      30-08-2023
+//
+//          Autor:      JUAN CARLOS GALEANO GARIBELLO
+//          Código:     202169777
+//
+//
+//         Descripción:
+//
+//
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+*/
+
+
+package ejemplopresentacion;
+
+
+
+public class EjemploPresentacion {
+
+   //Hola Muchachos very nice|hola
+    public static void main(String[] args) {
+                
+        System.out.println("Hola Mundo");
+        
+        ventanaPrincipal ventanaMenu = new ventanaPrincipal();
+        
+        ventanaMenu.setSize(600, 400);
+        ventanaMenu.setLocation(400,200);
+        ventanaMenu.setVisible(true);
+       
+        
+        
+    }
+    
+}
